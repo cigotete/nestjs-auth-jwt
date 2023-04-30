941c4721b33c5dc64d922c84ba4863a8868025b2
Camilo Figueroa <cigotete@gmail.com>
2023-04-30 18:21:18 -0500

    Checking if user already exists.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './users.model';
@@ -9,6 +9,10 @@ export class UsersService {
     @InjectModel('user') private readonly userModel: Model<UserDocument>,
   ) {}
   async createUser(username: string, password: string): Promise<User> {
+    const existingUser = await this.getUser({ username });
+    if (existingUser) {
+      throw new ConflictException('User already exists');
+    }
     return this.userModel.create({
       username,
       password,
