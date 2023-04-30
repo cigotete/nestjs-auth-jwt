39233d5a69ce7fb32f8f24eb696e73c45777cfee
Camilo Figueroa <cigotete@gmail.com>
2023-04-30 18:20:29 -0500

    Changes to check if parameters received are empty.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Param,
+  BadRequestException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './users.model';
 import * as bcrypt from 'bcrypt';
@@ -12,6 +19,12 @@ export class UsersController {
     @Body('password') password: string,
     @Body('username') username: string,
   ): Promise<User> {
+    if (!password) {
+      throw new BadRequestException('Password cannot be empty');
+    }
+    if (!username) {
+      throw new BadRequestException('Username cannot be empty');
+    }
     const saltOrRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltOrRounds);
     const result = await this.usersService.createUser(username, hashedPassword);
